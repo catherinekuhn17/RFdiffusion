81e0fe3dc809089e5a4b402506bb27cfc5769201
Catherine Kuhn <ckuhn@dev1.wynton.ucsf.edu>
2025-08-12 23:00:19 -0700

    adding how to

--- examples/design_switch.sh
+++ examples/design_switch.sh
@@ -2,24 +2,16 @@
 
 #!/bin/bash
 #$ -S /bin/bash
-#$ -o log_files/
-#$ -e log_files/
 #$ -cwd
 #$ -j y
 #$ -r y
 #$ -l mem_free=10G
 #$ -l h_rt=96:00:00
 
+conda activate SE3nv
 
-LD_PRELOAD=/wynton/home/kortemme/ckuhn/Desktop/kortemme/glibc-2.27/opt/glibc-2.27/lib/libm.so.6
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/wynton/home/kortemme/ckuhn/anaconda3/envs/SE3nv2/lib/
-export CUDA_VISIBLE_DEVICES=$SGE_GPU
-conda activate SE3nv2
-export HYDRA_FULL_ERROR=1
-
-
-INPUT_PDB1='input_pdbs/HH_SQR_centered2.pdb'
-INPUT_PDB2='input_pdbs/HH_TET_aligned2.pdb'
+INPUT_PDB1='input_pdbs/HH_SQR_centered.pdb'
+INPUT_PDB2='input_pdbs/HH_TET_aligned.pdb'
 
 
 CUTOFF=$1
@@ -35,7 +27,7 @@ NUM_DES=5
 OUT_FN="example_outputs/HH_45-55_contig1_5-8_contig2_45-55_cut${CUTOFF}_sw${SWITCH_PACE}_start${STARTING_PDB}_part${conserve_px0_part}_all${conserve_px0_all}/HH_45-55_contig1_5-8_contig2_45-55_cut${CUTOFF}_sw${SWITCH_PACE}_start${STARTING_PDB}_part${conserve_px0_part}_all${conserve_px0_all}"
 
 
-SWITCH_RT='input_rT/switch_rot2.npz'
+SWITCH_RT='input_rT/switch_rot.npz'
 
 python ../scripts/run_inference.py \
 	--config-name=switch \
@@ -48,8 +40,6 @@ python ../scripts/run_inference.py \
 	inference.switch_pace=$SWITCH_PACE \
 	inference.starting_pdb=$STARTING_PDB \
     inference.cutoff=$CUTOFF \
-	inference.switch=True \
-	inference.cutoff=$CUTOFF \
 	inference.num_designs=$NUM_DES \
 	inference.output_prefix=$OUT_FN \
 	inference.input_pdb=$INPUT_PDB1 \
