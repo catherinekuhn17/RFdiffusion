85d2dd325645fbcee5db34235d95911339a70462
Catherine Kuhn <ckuhn@dev1.wynton.ucsf.edu>
2025-08-12 22:42:16 -0700

    some updates

--- examples/design_switch.sh
+++ examples/design_switch.sh
@@ -1,3 +1,5 @@
+# designing a switch (assuming we have already define the rotation/translation for the switching behavior)
+
 #!/bin/bash
 #$ -S /bin/bash
 #$ -o log_files/
@@ -10,12 +12,14 @@
 
 
 LD_PRELOAD=/wynton/home/kortemme/ckuhn/Desktop/kortemme/glibc-2.27/opt/glibc-2.27/lib/libm.so.6
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/wynton/home/kortemme/ckuhn/anaconda3/envs/SE3nv2/lib/
 export CUDA_VISIBLE_DEVICES=$SGE_GPU
-conda activate SE3nv_new
+conda activate SE3nv2
 export HYDRA_FULL_ERROR=1
 
-INPUT_PDB1='input_pdbs/HH_SQR_centered.pdb'
-INPUT_PDB2='input_pdbs/HH_TET_aligned.pdb'
+
+INPUT_PDB1='input_pdbs/HH_SQR_centered2.pdb'
+INPUT_PDB2='input_pdbs/HH_TET_aligned2.pdb'
 
 
 CUTOFF=$1
@@ -26,22 +30,23 @@ conserve_px0_all=$5
 
 CONTIGS='contigmap.contigs=[45-55/A1-11/5-8/B1-11/45-55]'
 
-NUM_DES=10
+NUM_DES=5
 
 OUT_FN="example_outputs/HH_45-55_contig1_5-8_contig2_45-55_cut${CUTOFF}_sw${SWITCH_PACE}_start${STARTING_PDB}_part${conserve_px0_part}_all${conserve_px0_all}/HH_45-55_contig1_5-8_contig2_45-55_cut${CUTOFF}_sw${SWITCH_PACE}_start${STARTING_PDB}_part${conserve_px0_part}_all${conserve_px0_all}"
 
 
-SWITCH_RT='input_rT/switch_rot.npz'
+SWITCH_RT='input_rT/switch_rot2.npz'
 
 python ../scripts/run_inference.py \
-    --config-name=base_sw \
-	inference.write_trajectory=False \
+    --config-name=switch \
+	inference.write_trajectory=True \
     inference.recenter=False \
+	inference.switch=True \
+	inference.center_mot=False \
 	inference.conserve_px0_part=$conserve_px0_part \
     inference.conserve_px0_all=$conserve_px0_all \
 	inference.switch_pace=$SWITCH_PACE \
 	inference.starting_pdb=$STARTING_PDB \
-    inference.switch=True \
     inference.cutoff=$CUTOFF \
 	inference.num_designs=$NUM_DES \
 	inference.output_prefix=$OUT_FN \
--- helper_scripts/center_pdb.sh
+++ helper_scripts/center_pdb.sh
@@ -3,13 +3,13 @@
 #$ -o log_files/
 #$ -e log_files/
 
-conda activate SE3nv_new
+conda activate SE3nv2 #SE3nv_new
 
 FN1='../examples/input_pdbs/HH_SQR.pdb'
 FN2='../examples/input_pdbs/HH_TET.pdb'
 pdb1_resi='B1-11'
 pdb2_resi='B1-11'
-OUT_FOLDER='../examples/input_pdbs'
+OUT_FOLDER='../examples/input_pdbs' # out filename will be FN1+ 'centered' and FN2 + 'aligned'
 
 if [ ! -d $OUT_FOLDER ]; then
   mkdir -p $OUT_FOLDER
